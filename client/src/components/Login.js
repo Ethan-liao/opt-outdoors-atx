7acb350f72036f3ab708590bbdd9fa8b6deeaa32
Daniel Isham <danielisham1@gmail.com>
2017-09-12 19:22:43 -0500

    login fetch added

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -3,37 +3,66 @@ import React from 'react';
 // import { Form, FormGroup, Col, FormControl, Checkbox, Button, ControlLabel  } from 'react-bootstrap'
 
 class Login extends React.Component {
-  // constructor() {
-  //   super();
-  //   this.goToApp = this.goToApp.bind(this);
-  // }
+  constructor(props) {
+    super(props);
 
-  goToApp(event) {
+    this.handleSubmit = this.handleSubmit.bind(this);
+
+    this.state = {
+      email:'',
+      pasword:''
+    }
+
+  }
+
+  handleSubmit(event) {
     event.preventDefault();
-    console.log('event', event);
-    console.log('You Changed the URL');
+    // console.log('event', event);
     // first grab the text from the box
-    const email = this.storeInput.value;
-    console.log(email);
+    const email = this.email.value;
+    console.log('email:', email);
+    const password = this.password.value;
+    console.log('password:', password);
     // second we're going to transition from / to /store/:storeId
     // this.context.router.transitionTo('/app/1');
     // this.browserHistory.push('/app/1')
-    // console.log(this);
-    // return (
-    //   <Redirect to='/app/1' push />
-    // )
-    this.props.history.push('/app/1')
+    // this.props.history.push('/app/1')
+
+    fetch('/users/', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({email: email, password: password})
+    }).then(function(response) {
+      if (response.status >= 400) {
+        throw new Error('Bad respose from server');
+      }
+      // console.log(response.json());
+      return response.json();
+    }).then(function(data) {
+      console.log('data: ', data);
+      this.setState({user: data})
+    })
+    .catch(function(error) {
+      console.error(error);
+    });
+
   }
 
   render() {
     return (
       <div>
         <h4>Log In</h4>
-        <form className="" onSubmit={(e) => this.goToApp(e)}>
+        <form className="" onSubmit={(e) => this.handleSubmit(e)}>
           <h2>Email</h2>
-          <input type="text" defaultValue="example@example.com" ref={(input) => { this.storeInput = input}}/>
+          <input type="text" defaultValue="danielisham1@gmail.com" ref={(input) => {
+            this.email = input
+          }}/>
           <h2>Password</h2>
-          <input type="text" />
+          <input type="text" defaultValue="password" ref={(input) => {
+            this.password = input
+          }}/>
           <button type="submit">Sign In</button>
         </form>
         <h4>Register Here</h4>

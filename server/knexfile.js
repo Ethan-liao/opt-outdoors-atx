7387ffb139feb77cc8f82c67e827afe2127f3074
Daniel Isham <danielisham1@gmail.com>
2017-09-26 11:04:09 -0500

    adding postgres db on heroku

--- server/knexfile.js
+++ server/knexfile.js
@@ -4,19 +4,19 @@ module.exports = {
     client: 'pg',
     connection: {
       database: process.env.DATABASE_URL || 'opt_out_atx'
-    }
-  }
+    },
   //
   // test: {
   //   client: 'pg',
   //   connection: {
-  //     database: process.env.DATABASE_URL || 'reddit-clone-test'
+  //     database: process.env.DATABASE_URL || 'opt_out_atx_test'
   //   }
   // },
 
-  // production: {
-  //   client: 'pg',
-  //   connection: `${process.env.DATABASE_URL}?ssl=true`
-  // }
+  production: {
+    client: 'pg',
+    connection: `${process.env.DATABASE_URL}?ssl=true`
+  }
+}
 
 };
